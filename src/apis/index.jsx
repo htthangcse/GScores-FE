5cba9d2d11708cb4115f76c1a8b4e45fd971300e
htthangcse <hatienthangk22cse@gmail.com>
2025-06-09 00:38:15 +0700

    update

--- src/apis/index.jsx
+++ src/apis/index.jsx
@@ -1,20 +1,20 @@
 import axios from 'axios'
-import { API_ROOT } from '../utils/constant'
+const API_ROOT = 'http://localhost:3000'
 
 export const fetchStudentScoreDetailsAPI = async (studentId) => {
-  const response = await axios.get(`http://localhost:3000/students/score/${studentId}`)
+  const response = await axios.get(`${API_ROOT}/students/score/${studentId}`)
   console.log('res ', response.data)
   return response.data
 }
 
 export const fetchTopGroupAStudentsAPI = async () => {
-  const response = await axios.get('http://localhost:3000/students/report/topScores') 
+  const response = await axios.get(`${API_ROOT}/students/report/topScores`) 
   console.log('res ', response.data)
   return response.data
 }
 
 export const fetchScoreLevelsReportAPI = async () => {
-  const response = await axios.get('http://localhost:3000/students/report/levels') 
+  const response = await axios.get(`${API_ROOT}/students/report/levels`) 
   console.log('res ', response.data)
   return response.data
 }
